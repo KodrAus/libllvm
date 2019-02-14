897415519b6d647e2f5bb1a0467281bf21f0fda3
Ashley Mannix <ashleymannix@live.com.au>
2019-02-15 08:02:01 +1000

    OSX Builds (#1)

---
+++ ci/llvm-src.ps1
@@ -0,0 +1,28 @@
+if (Test-Path llvm-src)
+{
+    Remove-Item -Recurse -Force llvm-src
+}
+
+New-Item -ItemType Directory -Path llvm-src
+Push-Location llvm-src
+
+& git init
+& git remote add origin https://github.com/llvm-mirror/llvm.git
+
+if (Test-Path llvm-src)
+{
+    Write-Host "Checking out specific commit"
+
+    & git fetch origin $(Get-Content ../llvm-commit) --depth 1
+}
+else
+{
+    Write-Host "Checking out latest commit"
+    
+    & git fetch origin release_60 --depth 1
+    (& git rev-parse origin/release_60) | Out-File -FilePath ../llvm-commit
+}
+
+& git checkout FETCH_HEAD
+
+Pop-Location
---
+++ ci/osx-x64.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ -d llvm-osx-x64 ]; then rm -r llvm-osx-x64; fi
+
+if [ ! -d llvm-src ]; then
+    ./ci/llvm-src.sh
+fi
+
+mkdir llvm-osx-x64
+pushd llvm-osx-x64
+
+cmake -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_LINK_LLVM_DYLIB=ON -DBUILD_SHARED_LIBS=OFF ../llvm-src
+make
+
+ls lib
+
+popd
