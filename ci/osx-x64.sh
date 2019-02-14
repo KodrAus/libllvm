425c6d5c1ae93092bdf590eed9e9ebd67e10f9bf
Ashley Mannix <ashleymannix@live.com.au>
2019-02-14 15:03:38 +1000

    stub out an osx-x64 build

---
+++ ci/osx-x64.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [ -d osx-x64-src ]; then rm -r osx-x64-src; fi
+if [ -d osx-x64 ]; then rm -r osx-x64; fi
+
+git clone -b release_60 --depth 1 https://github.com/llvm-mirror/llvm.git osx-x64-src
+
+mkdir osx-x64
+pushd osx-x64
+
+cmake -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_LINK_LLVM_DYLIB=ON -DBUILD_SHARED_LIBS=OFF ../osx-x64-src
+make
+
+ls lib
+
+popd
