ea3624230930615a6e35379d0295a3459dbd0b0e
Ashley Mannix <ashleymannix@live.com.au>
2019-02-14 17:14:37 +1000

    fetch llvm source once

--- ci/osx-x64.sh
+++ ci/osx-x64.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-if [ -d osx-x64-src ]; then rm -r osx-x64-src; fi
-if [ -d osx-x64 ]; then rm -r osx-x64; fi
+if [ -d llvm-osx-x64 ]; then rm -r llvm-osx-x64; fi
 
-git clone -b release_60 --depth 1 https://github.com/llvm-mirror/llvm.git osx-x64-src
+mkdir llvm-osx-x64
+pushd llvm-osx-x64
 
-mkdir osx-x64
-pushd osx-x64
-
-cmake -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_LINK_LLVM_DYLIB=ON -DBUILD_SHARED_LIBS=OFF ../osx-x64-src
+cmake -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_LINK_LLVM_DYLIB=ON -DBUILD_SHARED_LIBS=OFF ../llvm-src
 make
 
 # mkdir lib
