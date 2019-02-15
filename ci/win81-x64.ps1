b5b60288f49084524758df6cb75f0e6a4b972d86
Ashley Mannix <ashleymannix@live.com.au>
2019-02-16 07:49:38 +1000

    fix on windows

--- ci/win81-x64.ps1
+++ ci/win81-x64.ps1
@@ -13,14 +13,13 @@ Push-Location llvm-win81-x64
 
 & cmake `
   -G"Visual Studio 15 2017" `
-  -DCMAKE_BUILD_TYPE=MinSizeRel \
   -DLLVM_INCLUDE_TESTS=OFF `
   -DLLVM_INCLUDE_BENCHMARKS=OFF `
   -DLLVM_INCLUDE_TOOLS=OFF `
   -DLLVM_OPTIMIZED_TABLEGEN=ON `
   ../llvm-src
-& cmake --build . --config Release
+& cmake --build . --config MinSizeRel
 
-ls Release/lib
+ls MinSizeRel/lib
 
 Pop-Location
