f8e1c8aa03302eb80496034e2ff6ba29738efa73
Ashley Mannix <ashleymannix@live.com.au>
2019-02-16 22:53:31 +1000

    more tools!

--- ci/win81-x64.ps1
+++ ci/win81-x64.ps1
@@ -1,5 +1,5 @@
 Push-Location ci/win81-x64
-ls "C:\Program Files (x86)\Microsoft Visual Studio\2017"
+ls "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build"
 ./build.bat
 Pop-Location
 
