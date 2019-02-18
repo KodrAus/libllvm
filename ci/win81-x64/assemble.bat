62390c74978da4011fe260287ff2a3316901aafe
Ashley Mannix <ashleymannix@live.com.au>
2019-02-18 16:57:17 +1000

    fix up vcvars cmd

--- ci/win81-x64/assemble.bat
+++ ci/win81-x64/assemble.bat
@@ -1,10 +1,10 @@
 set srcdir=%1
 set builddir=%2
 
-set vcvarsall="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
+set vcvars="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
-if exist %vcvarsall% (
-    @call %vcvarsall% amd64_x64
+if exist %vcvars% (
+    @call %vcvars%
 )
 
 msbuild /p:Configuration=Release /p:Platform=x64 /p:LLVM_SRC_DIR=%srcdir% /p:LLVM_BUILD_DIR=%builddir% libLLVM.vcxproj
