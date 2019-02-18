cdb0bb901a8b660b817f9da397dc12503f5cdab6
Ashley Mannix <ashleymannix@live.com.au>
2019-02-18 15:22:59 +1000

    try amd64_x64 to use Host64 compiler

--- ci/win81-x64/assemble.bat
+++ ci/win81-x64/assemble.bat
@@ -4,7 +4,7 @@ set builddir=%2
 set vcvarsall="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
 
 if exist %vcvarsall% (
-    @call %vcvarsall% amd64
+    @call %vcvarsall% amd64_x64
 )
 
 msbuild /p:Configuration=Release /p:Platform=x64 /p:LLVM_SRC_DIR=%srcdir% /p:LLVM_BUILD_DIR=%builddir% libLLVM.vcxproj
