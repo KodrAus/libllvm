05de865a0be55d22ab42d92f189ad75e9cca7a9a
Ashley Mannix <ashleymannix@live.com.au>
2019-02-21 06:18:53 +1000

    standardise on powershell for scripts (#7)

--- ci/llvm-src.ps1
+++ ci/llvm-src.ps1
@@ -20,9 +20,22 @@ else
     Write-Host "Checking out latest commit"
 
     & git fetch origin release_60 --depth 1
-    (& git rev-parse origin/release_60) | Out-File -FilePath ../llvm-commit
+
+    New-Item -ItemType File -Path ../llvm-commit
+    (& git rev-parse origin/release_60) | Add-Content -Path ../llvm-commit
 }
 
 & git checkout FETCH_HEAD
+
+$major = (Select-String -Pattern "set\(LLVM_VERSION_MAJOR ([0-9]*)\)" -Path CMakeLists.txt).Matches.Groups[1].Value
+$minor = (Select-String -Pattern "set\(LLVM_VERSION_MINOR ([0-9]*)\)" -Path CMakeLists.txt).Matches.Groups[1].Value
+$patch = (Select-String -Pattern "set\(LLVM_VERSION_PATCH ([0-9]*)\)" -Path CMakeLists.txt).Matches.Groups[1].Value
+
+if (Test-Path ../llvm-version)
+{
+    Remove-Item ../llvm-version
+}
+New-Item -ItemType File -Path ../llvm-version
+Add-Content -Value "$major.$minor.$patch" -Path ../llvm-version
 
 Pop-Location
