79f247bb50bab250ecb382a6bd5ce13c5ff6b20b
Ashley Mannix <ashleymannix@live.com.au>
2019-02-21 07:14:05 +1000

    ensure we use UTC ticks for ts component

--- ci/llvm-pkg.ps1
+++ ci/llvm-pkg.ps1
@@ -1,8 +1,7 @@
-if (Test-Path llvm-pkg)
+if (-not (Test-Path llvm-pkg))
 {
-    Remove-Item -Recurse -Force llvm-pkg
+    New-Item -ItemType Directory -Path llvm-pkg
 }
-New-Item -ItemType Directory -Path llvm-pkg
 
 $buildMeta = ""
 if (Test-Path llvm-version)
@@ -11,7 +10,7 @@ if (Test-Path llvm-version)
 }
 
 $prefix = (Select-String -Pattern "Version>([0-9]*\.[0-9]*\.[0-9]*)</Version" -Path libLLVM.csproj).Matches.Groups[1].Value
-$ts = [math]::Round((get-date).ticks / 10000000)
+$ts = [math]::Round(((get-date).ToUniversalTime()).ticks / 10000000)
 
 $version = "$prefix-dev.$ts"
 
