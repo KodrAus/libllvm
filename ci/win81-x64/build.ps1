295d18283df93bf76f6f87dc288f8e9da03a98e1
Ashley Mannix <ashleymannix@live.com.au>
2019-02-20 17:16:04 +1000

    move build scripts into their own folder

--- ci/win81-x64/build.ps1
+++ ci/win81-x64/build.ps1
@@ -27,8 +27,8 @@ $buildDir = "$(pwd)/llvm-win81-x64"
 
 Push-Location ci/win81-x64
 
-./build.bat $buildDir
-./assemble.bat $srcDir $buildDir
+./llvm-build.bat $buildDir
+./llvm-assemble.bat $srcDir $buildDir
 
 cp x64/MinSizeRel/libLLVM.dll $buildDir/LLVM.dll
 
